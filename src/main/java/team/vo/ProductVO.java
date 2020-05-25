9d3e99bcfcc18754f09ab4888d9a22c7b1d6ff4b
jhcool1988 <jhcool1988@gmail.com>
2020-05-25 16:01:31 +0900

    브랜치 생성

---
+++ src/main/java/team/vo/ProductVO.java
@@ -0,0 +1,20 @@
+package team.vo;
+
+public class ProductVO {
+    private String name;
+
+    public ProductVO() {
+    }
+
+    public ProductVO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+};
